96faeac287d62da39dada13e3730eea4f21d50aa
GabAlmeyda <gabrielalmeida7258@gmail.com>
2026-05-01 10:46:16 -0300

    feat: adição do favicon do site; adição de atributos aria-* e pequenas melhorias de acessibilidade; adição de arquivos 'robots.txt' e 'sitemap.xml' para melhoria de SEO

--- js/global.js
+++ js/global.js
@@ -1,7 +1,27 @@
+function toggleNavbarAriaAtt() {
+  const checkbox = document.getElementById("navbar-checkbox");
+  if (!checkbox.checked) {
+    checkbox.setAttribute("aria-label", "Abrir menu de navegação");
+  } else {
+    checkbox.setAttribute("aria-label", "Fechar menu de navegação");
+  }
+
+  const menuButton = document.getElementById("menu-button");
+  menuButton.setAttribute("aria-expanded", checkbox.checked);
+}
+
 function handleNavbarLinkClick() {
-  document.getElementById("navbar-checkbox").checked = false;
+  const checkbox = document.getElementById("navbar-checkbox");
+  if (checkbox.checked) {
+    checkbox.checked = false;
+    toggleNavbarAriaAtt();
+  }
 }
 
 document
   .querySelectorAll(".g-navbar-links li")
   .forEach((link) => link.addEventListener("click", handleNavbarLinkClick));
+
+document
+  .getElementById("navbar-checkbox")
+  .addEventListener("change", toggleNavbarAriaAtt);
